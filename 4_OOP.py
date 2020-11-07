10e4f0ee4df53cfb2eb512183be374e7f3aee8c8
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2020-11-07 18:57:29 +0300

    Update 4_OOP.py

--- 4_OOP.py
+++ 4_OOP.py
@@ -2,89 +2,51 @@ import random
 
 class Weapon:
     
-    def __init__(self, weapon_name, weapon_damage,
-                 weapon_price, weapon_abrasion):
-        self._name = weapon_name
-        self._damage = weapon_damage
-        self._price = weapon_price
-        self._abrasion = weapon_abrasion
-        
-    def foo(self, new_price):
-        self._price = new_price
-        return print("Родитель Weapon. Цена =", self._price)
-        
-class Steel_Weapon(Weapon):
+    def __init__(self, w_name, w_weight, w_damage):
+        self._name = w_name
+        self._weight = w_weight
+        self._damage = w_damage
     
-    def __init__(self, steel_weapon_type,
-                 weapon_name, weapon_damage,
-                 weapon_price, weapon_abrasion):
-        super().__init__(weapon_name, weapon_damage,
-                 weapon_price, weapon_abrasion)
-        self.__type = steel_weapon_type
+    def foo(self, new_damage):
+        self._damage -= new_damage
+        return print("Родитель Weapon. Урон =", self._damage)
+
+class Main_Weapon(Weapon):
     
-    def foo(self, new_price):
-        self.__price = 200
-        return print("Потомок Steel_Weapon. Цена =", self.__price)        
+    def __init__(self, mw_type, w_name, w_weight, w_damage):
+        super().__init__(w_name, w_weight, w_damage)
+        self.__type = mw_type
+        
+    def foo(self, new_damage):
+        self.__damage = 888
+        return print("Потомок Main_Weapon. Урон =", self.__damage)
 
-class Silver_Weapon(Weapon):
+class Aux_Weapon(Weapon):
     
-    def __init__(self, silver_weapon_type,
-                 silver_weapon_ability,
-                 weapon_name, weapon_damage,
-                 weapon_price, weapon_abrasion):
-        super().__init__(weapon_name, weapon_damage,
-                 weapon_price, weapon_abrasion)
-        self.__type = silver_weapon_type
-        self.__ability = silver_weapon_ability
+    def __init__(self, aux_w_ability, w_name, w_weight, w_damage):
+        super().__init__(w_name, w_weight, w_damage)
+        self.__type = aux_w_ability
         
-    def foo(self, new_price):
-        self.__price = 300
-        return print("Потомок Silver_Weapon. Цена =", self.__price)
-        
-grabli = Weapon("Грабли", 100, 50, 87)
-grabli.foo(70)
+    def foo(self, new_damage):
+        self.__damage = 555
+        return print("Потомок Aux_Weapon. Урон =", self.__damage)
 
-arondit = Silver_Weapon("Реликт", "Накопление урона", "Арондит",
-                       750, 2100, 0)
-arondit.foo(200)
-
-burning_rose_sword = Steel_Weapon("Предмет мастера",
-                                  "Меч рыцарей пылающей розы",
-                                  300, 1700, 15)
-burning_rose_sword.foo(150)
-
-# *****************
-
-# steel_swords
-ard_aenye = Steel_Weapon("Реликт", "Ард Эйн", 320, 500, 15) #1
-angivar = Steel_Weapon("Реликт", "Ангивар", 250, 600, 0) #2
-bahnshee = Steel_Weapon("Реликт", "Банши", 300, 550, 54) #3
-long_claw = Steel_Weapon("Обычный", "Длинный коготь", 420, 650, 10) #4
-ancient_elf_sword = Steel_Weapon("Магический", "Древний эльфский меч",
-                                 510, 1000, 0) #5
-
-# silver_swords
-addan_deit = Silver_Weapon("Реликт", "Повышенное кровотечение",
-                           "Аддан Дейт", 500, 800, 72) #1
-anaphema = Silver_Weapon("Реликт", "Шанс вызвать горение",
-                         "Анафема", 480, 720, 24) #2
-mountain_tooth = Silver_Weapon("Мастерский", "Доп. урон при ударе",
-                               "Горный зуб", 370, 600, 15) #3
-meltyh = Silver_Weapon("Магический", "Шанс критического удара",
-                       "Мелтих", 420, 1000, 0) #4
-tuneller = Silver_Weapon("Обычный", "Шанс критического удара",
-                         "Проходчик", 500, 1550, 0) #5
-                         
-armory = [ard_aenye, addan_deit, angivar, anaphema,
-          bahnshee, mountain_tooth, long_claw, meltyh,
-          ancient_elf_sword, tuneller]
-          
-inventory = []
-
-print()
+swords = []
 
 for i in range(10):
-    inventory.append(armory[random.randint(0, 9)])
+    if random.randint(1,10) % 2 == 0: 
+        main_sword = Main_Weapon(random.randint(1, 10), # mw_type
+                            random.randint(1, 10),      # w_name
+                            random.randint(1, 5),       # w_weight
+                            random.randint(200, 600))   # w_damage
+        swords.append(main_sword)
+        
+    else:
+        aux_sword = Aux_Weapon(random.randint(1, 10), # aux_w_ability
+                            random.randint(1, 10),    # w_name
+                            random.randint(1, 5),     # w_weight
+                            random.randint(200, 600)) # w_damage
+        swords.append(aux_sword)
 
-for Weapon in inventory:
+for Weapon in swords:
     Weapon.foo(10)
